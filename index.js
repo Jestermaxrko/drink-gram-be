f5ce1035157b220d628ace7646f5e02c64ac3303
Jester <jestermaxrko@gmail.com>
2019-04-01 23:07:37 +0300

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 app.use('/', routes);
 
 app.set('port', process.env.PORT || 3000);
